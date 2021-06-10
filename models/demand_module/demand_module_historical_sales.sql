93d79b13ffcabbb6ae15ed72f37413af399a6cc1
egmontecom <82501261+egmontecom@users.noreply.github.com>
2021-06-10 10:21:17 +0200

    Update demand_module_historical_sales.sql

--- models/demand_module/demand_module_historical_sales.sql
+++ models/demand_module/demand_module_historical_sales.sql
@@ -3,6 +3,7 @@ transaction_date,
 transaction_id,
 product_sku,
 product_line_price_lcy,
-product_line_price_ccy
-product_quantity
+product_line_price_ccy,
+product_quantity,
+category_description,
 FROM {{ref('transformed_WP_sales')}}
