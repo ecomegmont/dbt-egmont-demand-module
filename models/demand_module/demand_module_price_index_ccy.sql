a35f00602b9b065d098ba8609e99e4e56aea6cad
Tiselius <HMMATI@egmont.com>
2021-06-10 15:51:37 +0200

    .

--- models/demand_module/demand_module_price_index_ccy.sql
+++ models/demand_module/demand_module_price_index_ccy.sql
@@ -5,7 +5,6 @@ calculation_date,
 SPLIT(product_id, ':')[OFFSET(3)] product_sku,
 product_price,
 price_benchmark_value,
-price_index as index,
 price_benchmark_currency
 FROM {{ref('price_index')}}
 WHERE
