e71c60b382247682e7f80f65970026ebb3e0538f
egmontecom <82501261+egmontecom@users.noreply.github.com>
2021-06-10 09:52:04 +0200

    Update demand_module_historical_sales.sql
    updated product_line without double underscore

--- models/demand_module/demand_module_historical_sales.sql
+++ models/demand_module/demand_module_historical_sales.sql
@@ -2,8 +2,7 @@ SELECT
 transaction_date,
 transaction_id,
 product_sku,
-product__line_price_lcy,
-product__line_price_ccy
+product_line_price_lcy,
+product_line_price_ccy
 product_quantity
 FROM {{ref('transformed_WP_sales')}}
-
