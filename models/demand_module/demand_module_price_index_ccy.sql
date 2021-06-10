fe62ffa98987839db8a18220d84cf333713cb5cf
Tiselius <HMMATI@egmont.com>
2021-06-10 15:25:30 +0200

    break down & drop  product_id

--- models/demand_module/demand_module_price_index_ccy.sql
+++ models/demand_module/demand_module_price_index_ccy.sql
@@ -1,5 +1,12 @@
-SELECT 
-*
+SELECT DISTINCT
+SPLIT(product_id, ':')[OFFSET(2)] country,
+brand,
+calculation_date,
+SPLIT(product_id, ':')[OFFSET(3)] product_sku,
+product_price,
+price_benchmark_value,
+price_index,
+price_benchmark_currency
 FROM {{ref('price_index')}}
 WHERE
 price_benchmark_currency = {{ var('company_currency') }}
