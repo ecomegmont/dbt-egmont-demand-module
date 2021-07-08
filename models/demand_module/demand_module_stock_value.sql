d53bcddb640f515bbb59629b8c11786ff97471d9
hmmoek <hmmoek@users.noreply.github.com>
2021-07-08 09:00:32 +0200

    added is_in_stock

--- models/demand_module/demand_module_stock_value.sql
+++ models/demand_module/demand_module_stock_value.sql
@@ -3,5 +3,9 @@
 SELECT 
 stock_date,
 product_sku,
-stock_qty
+stock_qty,
+case 
+    when stock_qty=0 then 0
+    when stock_qty>0 then 1
+end as is_in_stock
 FROM {{ref('transformed_erp_stockvaluebyday')}}
