7f2ba0ff7933bba60465fbacac85f5fcb9727668
adam.tiselius@egmont.se <adam.tiselius@egmont.se>
2021-08-16 08:59:47 +0200

    uppdated to read from new table.

--- models/demand_module/demand_module_stock_value.sql
+++ models/demand_module/demand_module_stock_value.sql
@@ -1,16 +1,11 @@
 
 
 SELECT 
-a.stock_date,
-a.product_sku,
-a.stock_qty,
-case 
-    when stock_qty=0 then 0
-    when stock_qty>0 then 1
-end as is_in_stock,
-b.product_brand 
-FROM {{ref('transformed_erp_stockvaluebyday')}} as a
-left join {{ref('transformed_product_sku_to_brand')}} as b
-on a.product_sku=b.product_sku
+stock_date,
+product_sku,
+sales_product_description,
+stock_qty,
+FROM {{ref('all_products_stock_value_by_day')}}
+
 
 
