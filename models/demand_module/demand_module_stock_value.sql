57be8a6afb5d1011d64247422bec0a1048908441
adam.tiselius@egmont.se <adam.tiselius@egmont.se>
2021-08-16 14:27:33 +0200

    fix after review

--- models/demand_module/demand_module_stock_value.sql
+++ models/demand_module/demand_module_stock_value.sql
@@ -1,7 +1,11 @@
 SELECT stock_date,
-product_sku,
-sales_product_description,
-stock_qty,
+    product_sku,
+    sales_product_description,
+    stock_qty,
+    case 
+     when stock_qty=0 then 0
+	 when stock_qty>0 then 1
+    end as is_in_stock
 FROM {{ref('all_products_stock_value_by_day')}}
 
 
