69beb08be7c2d669efb431e1b60e7e3d8fd37c82
adam.tiselius@egmont.se <adam.tiselius@egmont.se>
2021-08-17 15:42:13 +0200

    fix

--- models/demand_module/demand_module_active_products.sql
+++ models/demand_module/demand_module_active_products.sql
@@ -15,12 +15,8 @@ product_sku,
 sales_product_description,
 stock_qty,
 weighted_stock_30_active_flag,
-CASE 
-    WHEN weighted_stock_30_active_flag > 0 then 1
-    ELSE 0
-    END as active_product
+(weighted_stock_30_active_flag <> 0)  as active_product
  FROM BASE
-
 
 
 
