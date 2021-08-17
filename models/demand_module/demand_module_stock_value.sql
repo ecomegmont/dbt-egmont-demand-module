b59cc94325d5160a132e50a366a41094bcdd16f5
adam.tiselius@egmont.se <adam.tiselius@egmont.se>
2021-08-17 10:39:18 +0200

    added 0 for lesser then

--- models/demand_module/demand_module_stock_value.sql
+++ models/demand_module/demand_module_stock_value.sql
@@ -4,6 +4,7 @@ SELECT stock_date,
     stock_qty,
     case 
      when stock_qty=0 then 0
+     When stock_qty< then 0
 	 when stock_qty>0 then 1
     end as is_in_stock
 FROM {{ref('all_products_stock_value_by_day')}}
