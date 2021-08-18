81d862bec3292c354c6f19ad65f055362eafdc84
adam.tiselius@egmont.se <adam.tiselius@egmont.se>
2021-08-18 09:29:54 +0200

    fix code fix schema and description

--- models/demand_module/demand_module_active_products.sql
+++ models/demand_module/demand_module_active_products.sql
@@ -7,7 +7,6 @@ WITH BASE AS (
     FROM {{ref('demand_module_stock_value')}}
     WINDOW w AS (PARTITION BY product_sku ORDER BY UNIX_DATE(stock_date) ASC 
     RANGE BETWEEN 30 PRECEDING AND 0 FOLLOWING)
-    ORDER BY stock_date DESC 
 )
 
 SELECT stock_date,
@@ -15,10 +14,8 @@ product_sku,
 sales_product_description,
 stock_qty,
 weighted_stock_30_active_flag,
-CASE 
-    WHEN weighted_stock_30_active_flag > 0 then 1
-    ELSE 0
-    END as active_product
+(weighted_stock_30_active_flag <> 0)  as active_product
  FROM BASE
+ ORDER BY stock_date DESC 
 
 
