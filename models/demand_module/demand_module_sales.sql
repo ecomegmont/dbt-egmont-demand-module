952b4909f1ada245e30b18e8fa877b10e9637a03
aTiselius <80458162+aTiselius@users.noreply.github.com>
2021-09-27 12:51:56 +0200

    allot of conversions rates

--- models/demand_module/demand_module_sales.sql
+++ models/demand_module/demand_module_sales.sql
@@ -1,3 +1,5 @@
+WITH BASE AS (
+
 SELECT
     EXTRACT(date from order_date) as order_dt,
     market,
@@ -27,3 +29,54 @@ FROM {{ref('transformed_WP_sales')}} a
         on a.product_sku = b.product_sku
             and a.market_name = b.Pricelist
     WHERE NOT a.product_sku = '{{ var("removes") }}'
+
+),     currency_table AS (
+        {# Extract join date in currency table #}
+        SELECT  date,
+                CAD_rate,
+                CHF_rate,
+                DKK_rate,
+                JPY_rate,
+                KRW_rate,
+                NOK_rate,
+                PLN_rate,
+                AUD_rate,
+                CNY_rate,
+                EUR_rate,
+                GBP_rate,
+                USD_rate,
+                CONCAT(EXTRACT(YEAR FROM date),'-',LPAD(CAST(EXTRACT(MONTH from date) as string),2,'0'),'-',LPAD(CAST(EXTRACT(DAY from date) as string),2,'0')) as cuDate
+                FROM {{ref('stg__navision_currencies_to_sek')}}
+        ),  final as (
+            SELECT
+            order_dt,
+                market,
+                market_name,
+                country_iso_2_code,
+                order_discount_code,
+                order_number,
+                a.product_sku,
+                product_name,
+                product_quantity,
+                discount_amount, --add converted
+                DiscountPercentage,
+                product_line_price_lcy,
+                product_line_price_ccy,
+                price_standard_current_pricelist,  --add converted
+                price_standard_unit_current_pricelist,  --add converted
+                top_level_price_standard,  --add converted
+                top_level_price_standard_unit,  --add converted
+                top_level_price_last_updated,
+                pricelist_name,
+                a.pricelist_id,
+                category_description,
+                brand_name,
+                line_number,
+                b.*
+                from BASE a
+                    left join 
+                        currency_table b
+                            on a.order_date = b.cuDate
+        )
+
+        SELECT * FROM final
