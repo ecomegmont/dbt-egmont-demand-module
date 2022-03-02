ed8c2bb8eb026738d0e9e8cc9c98bcb22b1ce1d8
adam.tiselius@egmont.se <adam.tiselius@egmont.se>
2022-03-02 08:48:20 +0100

    Made clear the fields are named correctly for Data science Merge

--- models/demand_module/demand_module_sku_level.sql
+++ models/demand_module/demand_module_sku_level.sql
@@ -1,3 +1,5 @@
-select *,
+select * EXCEPT (product_sku, revenue_ccy),
+product_sku as shipped_product_sku,
+revenue_ccy as revenue_lcy,
     case when sold_product_sku != product_sku then true else false END as virtual_product
  from {{ ref('storm_sales_sku_level') }}
