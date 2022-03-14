3635eb3311392bd073ebd47fa807f39ad11bd059
adam.tiselius@egmont.se <adam.tiselius@egmont.se>
2022-03-14 14:42:01 +0100

    descriptions

--- models/demand_module/demand_module_purchase_orders.sql
+++ models/demand_module/demand_module_purchase_orders.sql
@@ -1,2 +1,5 @@
 select * from {{ ref('input_purchase_order') }}
 where expected_arrival_date is not null
+
+--excluding values that not have a expected date. 
+--Dates get filled in when the order gets registered in ERP.
